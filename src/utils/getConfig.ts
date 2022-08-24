18c61aea677c1e046e4b32d5352b9f083cc506d8
JS-banana <sss213018@163.com>
2022-08-24 14:29:01 +0800

    feat(config): 🏷️ 对config添加类型定义

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -7,7 +7,7 @@ import fs from 'fs'
 import path from 'path'
 import YAML from 'yaml'
 
-export const getConfig = () => {
+export const getConfig = (): IConfigProps => {
   console.log('配置文件路径：', path.resolve(process.cwd(), './config.yml'))
   const file = fs.readFileSync(path.resolve(process.cwd(), './config.yml'), 'utf8')
   return YAML.parse(file)

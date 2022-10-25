f516b0621e2bf683a9cabf44bb4f8c61e7d29719
sunss <sss213018@163.com>
2022-10-25 22:26:54 +0800

    chore: 注释代码恢复

--- src/libs/LoveMsg/goodMorning.ts
+++ src/libs/LoveMsg/goodMorning.ts
@@ -5,49 +5,49 @@
 import API from '../../api/loveMsg'
 import { getConfig } from '../../utils/getConfig'
 import { wxNotify } from '../WxNotify'
-// import { textTemplate } from './templates/text'
+import { textTemplate } from './templates/text'
 import { textCardImportantTips, textCardTemplate } from './templates/textcard'
 
 const CONFIG = getConfig().loveMsg
 
 // 美丽短句
-// const goodWord = async () => {
-//   try {
-//     // 并行请求，优响相应
-//     const dataSource = await Promise.allSettled([
-//       API.getSaylove(), // 土味情话
-//       API.getCaihongpi(), // 彩虹屁
-//       API.getOneWord(), // 一言
-//       API.getSongLyrics(), // 最美宋词
-//       API.getOneMagazines(), // one杂志
-//       API.getNetEaseCloud(), // 网易云热评
-//       API.getDayEnglish(), // 每日英语
-//     ])
+const goodWord = async () => {
+  try {
+    // 并行请求，优响相应
+    const dataSource = await Promise.allSettled([
+      API.getSaylove(), // 土味情话
+      API.getCaihongpi(), // 彩虹屁
+      API.getOneWord(), // 一言
+      API.getSongLyrics(), // 最美宋词
+      API.getOneMagazines(), // one杂志
+      API.getNetEaseCloud(), // 网易云热评
+      API.getDayEnglish(), // 每日英语
+    ])
 
-//     // 过滤掉异常数据
-//     const [sayLove, caiHongpi, oneWord, songLyrics, oneMagazines, netEaseCloud, dayEnglish] = dataSource.map(
-//       (n) => (n.status === 'fulfilled' ? n.value : null)
-//     )
+    // 过滤掉异常数据
+    const [sayLove, caiHongpi, oneWord, songLyrics, oneMagazines, netEaseCloud, dayEnglish] = dataSource.map(
+      (n) => (n.status === 'fulfilled' ? n.value : null)
+    )
 
-//     // 对象写法
-//     const data: any = {
-//       sayLove,
-//       caiHongpi,
-//       oneWord,
-//       songLyrics,
-//       oneMagazines,
-//       netEaseCloud,
-//       dayEnglish,
-//     }
+    // 对象写法
+    const data: any = {
+      sayLove,
+      caiHongpi,
+      oneWord,
+      songLyrics,
+      oneMagazines,
+      netEaseCloud,
+      dayEnglish,
+    }
 
-//     const template = textTemplate(data)
-//     console.log('goodWord', template)
+    const template = textTemplate(data)
+    console.log('goodWord', template)
 
-//     wxNotify(template)
-//   } catch (error) {
-//     console.log('goodWord:err', error)
-//   }
-// }
+    wxNotify(template)
+  } catch (error) {
+    console.log('goodWord:err', error)
+  }
+}
 
 // 天气信息
 const weatherInfo = async () => {
@@ -79,5 +79,5 @@ const weatherInfo = async () => {
 // goodMorning
 export const goodMorning = async () => {
   await weatherInfo()
-  // await goodWord()
+  await goodWord()
 }
